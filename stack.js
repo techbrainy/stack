4b7b349a7cad0419cf566e7dc8ada3d6fa6aca9a
Ashwini sarode <techbrainy97@gmail.com>
2022-02-21 22:35:19 +0100

    second commit

--- stack.js
+++ stack.js
@@ -2,7 +2,23 @@ class stack_functions {
 
 
     constructor() {
-        this.items = [];
+        this.stackJS = [];
 
     }
+
+    push(data) {
+        return this.stackJS.push(data);
+
+    }
+    pop(element) {
+
+
+    }
+    peek() {
+
+    }
+
 }
+const stackelements = new stack_functions();
+stackelements.push(4);
+console.log(stackelements.stackJS)
