2f94e23c1c7989c269b87b214ac2a31c56d7c1b2
Ashwini sarode <techbrainy97@gmail.com>
2022-02-24 17:07:51 +0100

    push pop code modified

--- stack.js
+++ stack.js
@@ -7,12 +7,14 @@ class stack_functions {
     }
 
     push(data) {
-        return this.stackJS.push(data);
+
+
+        return this.stackJS[this.stackJS.length] = data;
+
 
     }
-    pop(element) {
-
-
+    pop() {
+        return this.stackJS[this.stackJS.length--]
     }
     peek() {
 
@@ -21,4 +23,10 @@ class stack_functions {
 }
 const stackelements = new stack_functions();
 stackelements.push(4);
+console.log(stackelements.stackJS)
+stackelements.push(6);
+console.log(stackelements.stackJS)
+stackelements.push(8);
+console.log(stackelements.stackJS)
+stackelements.pop();
 console.log(stackelements.stackJS)
